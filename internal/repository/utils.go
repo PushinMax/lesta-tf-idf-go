27802aeac2c477c5606d487d4d6efd2a24aef6f0
Pushin Maxim <macsime12321@gmail.com>
2025-06-14 21:23:46 +0300

    fixes schema

--- internal/repository/utils.go
+++ internal/repository/utils.go
@@ -22,3 +22,31 @@ type FileDocument struct {
     Stats   []schema.WordStat `bson:"stat"`
     Length  int               `bson:"len"`
 }
+
+type Document struct {
+    ID      primitive.ObjectID `bson:"_id,omitempty"`
+    FileID  string            `bson:"file_id"`
+    Name    string            `bson:"file_name"`
+    UserID  string            `bson:"user_id"`
+    Content string            `bson:"file"`
+    Stats   []schema.WordStat `bson:"stat"`
+    IsValid bool              `bson:"isvalid"`
+    Length  int               `bson:"len"`
+    Words map[string]int      `bson:"words"`
+    Collections []string      `bson:"collections"`
+}
+
+type Collection struct {
+    ID      primitive.ObjectID `bson:"_id,omitempty"`
+    Name    string            `bson:"name"`
+    UserID  string            `bson:"user_id"`
+    Stats   []schema.WordStat `bson:"stat"`
+    IsValid bool              `bson:"isvalid"`
+    Length  int               `bson:"len"`
+    Words map[string]struct{
+        amount_w int `bson:"amount_w"`
+        amount_d int `bson:"amount_d"`
+    }      `bson:"words"`
+    DocumentsID []string      `bson:"documents"`
+}
+
